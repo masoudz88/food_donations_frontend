9540f133c12d18bd458cce69819a55e7100519e7
Masoud Zare <57842159+masoudz88@users.noreply.github.com>
2021-03-16 01:35:46 -0230

    Delete my-app directory

--- my-app/src/defaults.jsx
+++
@@ -1,4 +0,0 @@
-const helloworld_en = "Hello World";
-const helloworld_fa = "salam donya!";
-export default helloworld_en;
-export { helloworld_fa };
--- my-app/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import Home from "./Home";
-import Login from "./Login";
-
-ReactDom.render(<Login />, document.getElementById("root"));
