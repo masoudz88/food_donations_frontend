cdb205a5cf51b2e74ab5c68bc9a9b888dfd03d5c
masoudz88 <masoudz@mun.ca>
2021-09-06 19:57:01 -0230

    fix the height of landing page

--- waste-management/src/Landingpage.js
+++ waste-management/src/Landingpage.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import LandingNav from './navbar/LandingNav';
+import React from "react";
+import LandingNav from "./navbar/LandingNav";
 
 const Landingpage = () => {
-    return (
-        <div >
-          <LandingNav/>
-          <div className="landingpage"
-  style={{
-    background: `url('${process.env.PUBLIC_URL}/logos/donations.jpg')`,    
-    height: "100vh",  
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',    
-    
-  }}
->
-    <h1 className="heading">
-     You are the solution,<br/> to the problem of hunger in the classroom</h1>
-</div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <LandingNav />
+      <div
+        className="landingpage"
+        style={{
+          background: `url('${process.env.PUBLIC_URL}/logos/donations.jpg')`,
+          height: "60vh",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <h1 className="heading">
+          You are the solution,
+          <br /> to the problem of hunger in the classroom
+        </h1>
+      </div>
+    </div>
+  );
+};
 export default Landingpage;
